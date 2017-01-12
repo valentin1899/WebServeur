8851716b1be3ad40dff2ba42d39ed50034c19a90
valentin <valentin@lajeunesse.fr>
2017-01-12 09:38:32 +0100

    Ajout du projet testProject

---
+++ testProject/api/controllers/UserController.js
@@ -0,0 +1,25 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+      moncompte: function (req, res){
+      var data = {};
+
+      data.medata ={
+      title: "Bonjour, bienvenue",
+      content: "Vous etes connecté"
+      };
+     //return res.view("moncompte");
+
+     console.log(req.user);
+
+     return res.view("moncompte", data);
+
+      }
+};
+
